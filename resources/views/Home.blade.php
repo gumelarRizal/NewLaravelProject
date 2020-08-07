1adc0e9cc2f2993fb3b2cfcc401106cf5cc51457
RizalGumelar <mrizalgumelar@gmail.com>
2020-08-07 18:21:29 +0700

    First Commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,53 @@
+@extends('Layouts.MainLayout')
+@section('Content')
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>Chart</h3>
+                <p>Income</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href="" class="small-box-footer">More Info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>53%</h3>
+                <p>Passive Income</p>
+            </div>
+            <div class="icon">
+                <i class="far fa-money-bill-alt"></i>
+            </div>
+            <a href="" class="small-box-footer">More Info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>30%</h3>
+                <p>Loss</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-sort-amount-down"></i>
+            </div>
+            <a href="" class="small-box-footer">More Info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-primary">
+            <div class="inner">
+                <h3>70%</h3>
+                <p>Profit</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-sort-amount-up-alt"></i>
+            </div>
+            <a href="" class="small-box-footer">More Info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
+@endsection
